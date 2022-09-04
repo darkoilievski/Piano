68304b0c80bbc78c032c3a3ae1d3ff817e6c72bb
darkoilievski <darkoilievski@yahoo.com>
2022-09-04 20:46:09 +0200

    changed the readme directory

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 window.addEventListener("keydown", (e) => {
   const pressedKey = e.key;
-  let keyLetter = document.querySelector(
-    `div[data-key="${pressedKey}"]`
-  );
+  let keyLetter = document.querySelector(`div[data-key="${pressedKey}"]`);
   if (!keyLetter) return;
-  let audio = document.querySelector(
-    `audio[data-key="${pressedKey}"]`
-  );
+  let audio = document.querySelector(`audio[data-key="${pressedKey}"]`);
 
   audio.currentTime = 0;
   audio.play();
